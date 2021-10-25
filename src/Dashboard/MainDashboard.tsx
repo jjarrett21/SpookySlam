41a7c48cc3fa57b047ec95a97643cdf8af668b0e
jjarrett21 <james.jarrett@okstate.edu>
2021-10-25 12:28:14 -0500

    adds display names

--- src/Dashboard/MainDashboard.tsx
+++ src/Dashboard/MainDashboard.tsx
@@ -161,3 +161,5 @@ export const MainDashboard: FC = () => {
     </div>
   );
 };
+
+MainDashboard.displayName = "MainDashboard";
--- src/Voting/Voting.tsx
+++ src/Voting/Voting.tsx
@@ -5,3 +5,5 @@ import { ChangeEvent, FC, SyntheticEvent, useState } from "react";
 export const Voting: FC = () => {
   return <div>VOTES GO HERE</div>;
 };
+
+Voting.displayName = "Voting";
