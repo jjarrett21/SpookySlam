f3d74c874eaa954de30608a004c0d881ffa66ce0
jjarrett21 <james.jarrett@okstate.edu>
2021-10-25 22:01:58 -0500

    better css and using uuid

--- src/Voting/Voting.tsx
+++ src/Voting/Voting.tsx
@@ -2,17 +2,47 @@
 import { jsx, css } from "@emotion/react";
 import { FC, useEffect, useState } from "react";
 import { db } from "../firebase/fireabse";
-import {
-  collection,
-  query,
-  where,
-  getDocs,
-  onSnapshot,
-  DocumentData,
-} from "firebase/firestore";
+import { collection, query, getDocs, DocumentData } from "firebase/firestore";
 import { defaultFontStyle } from "../tokens/functions";
-import { Contestant } from "./types";
 import { Card } from "react-bootstrap";
+import { v4 as uuidv4 } from "uuid";
+
+const cardStyles = (url: string) => css`
+  background-color: #ffffff;
+  position: relative;
+  cursor: pointer;
+  height: 110px;
+  width: 165px;
+  border-radius: 3px;
+  border: 2px solid black;
+
+  font-family: Spooky;
+
+  background-image: url(${url});
+  background-position: center center;
+  background-size: auto 100%;
+  background-color: $FFFF00;
+  background-repeat: no-repeat;
+  box-shadow: -4px 0px 6px rgba(0, 0, 0, 0.3);
+
+  &:hover {
+    background-color: $FFFF00;
+  }
+`;
+
+const innerCardStyles = css`
+  height: 100%;
+  width: 100%;
+  position: relative;
+`;
+
+const wrapperStyles = css`
+position: absolute;
+top: 20%;
+margin-top: -50px;
+width: 100%;
+height: 100%;
+​`;
 
 export const Voting: FC = () => {
   const [contestants, setContestants] = useState<DocumentData[]>([]);
@@ -35,13 +65,22 @@ export const Voting: FC = () => {
   return (
     <div>
       <h1 css={defaultFontStyle}>VOTES GO HERE</h1>
-      {contestants?.map((c) => (
-        <Card key={`${c.name}`}>
-          <Card.Title>{c.name}</Card.Title>
-          <Card.Img></Card.Img>
-          <Card.Text>{c.votes}</Card.Text>
+      <div css={wrapperStyles}>
+        <Card>
+          {contestants?.map((c) => (
+            <div key={`${uuidv4()}`}>
+              <Card.Text>Name: {c.name}</Card.Text>
+              <Card.Img css={cardStyles(c.url)}>
+                {/* <div css={cardStyles(c.url)}>
+                  <div css={innerCardStyles} />
+                </div> */}
+              </Card.Img>
+
+              <Card.Text>Votes: {c.votes}</Card.Text>
+            </div>
+          ))}
         </Card>
-      ))}
+      </div>
     </div>
   );
 };
